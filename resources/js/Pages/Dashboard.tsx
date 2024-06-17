ed1d853203bcc0759b339d30b1efa05e24458382
Hector Centeno <hector@dommo.mx>
2024-06-17 17:42:48 -0600

    add space between articles

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -60,19 +60,19 @@ export default function Dashboard({ auth, goals, todos, clients, completed }: Pr
 
         <div className='bg-white dark:bg-slate-500 my-4'>
           <div className='mx-auto max-w-7xl p-4 space-y-4'>
-            <div>
+            <div className='space-y-1'>
               <h3 className='dark:text-orange-400 text-orange-700 font-semibold'>Milagro</h3>
               <TodosList todos={todos.filter(f => f.priority == 1)} setSelected={setSelected} selected={selected} />
             </div>
-            <div>
+            <div className='space-y-1'>
               <h3 className='dark:text-lime-400 text-lime-700 font-semibold'>Prioridad</h3>
               <TodosList todos={todos.filter(f => f.priority == 2)} setSelected={setSelected} selected={selected} />
             </div>
-            <div>
+            <div className='space-y-1'>
               <h3 className='text-stone-700 font-semibold'>Algún día</h3>
               <TodosList todos={todos.filter(f => f.priority == 3)} setSelected={setSelected} selected={selected} />
             </div>
-            <div>
+            <div className='space-y-1'>
               <h3 className='text-gray-600 font-semibold'>Delegar</h3>
               <TodosList todos={todos.filter(f => f.priority == 4)} setSelected={setSelected} selected={selected} />
             </div>
